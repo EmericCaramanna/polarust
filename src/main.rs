5a75a66779f8ba489c820ea3562381606b93a251
Emeric <emeric.caramanna@epitech.eu>
2024-07-04 12:32:21 +0200

    Complete change of grarph lib
    Changed from piston to ggez which seems simpler and more appropriate for this project

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,93 @@
-extern crate glutin_window;
-extern crate graphics;
-extern crate opengl_graphics;
-extern crate piston;
 
-use glutin_window::GlutinWindow as Window;
-use opengl_graphics::{GlGraphics, OpenGL};
-use piston::event_loop::{EventSettings, Events};
-use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
-use piston::window::WindowSettings;
+use ggez::{
+    event, glam::*, graphics::{self, Color}, input::keyboard::KeyCode, Context, GameResult
+};
 
-pub struct App {
-    gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64,  // Rotation for the square.
+struct MainState {
+    bird: Bird,
 }
 
-impl App {
-    fn render(&mut self, args: &RenderArgs) {
-        use graphics::*;
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let circle = graphics::Mesh::new_circle(
+            ctx,
+            graphics::DrawMode::fill(),
+            vec2(100., 0.),
+            10.0,
+            2.0,
+            Color::WHITE,
+        )?;
+        let bird = Bird::new(circle);
 
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-
-        let square = rectangle::square(0.0, 0.0, 50.0);
-        let rotation = self.rotation;
-        let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
-
-        self.gl.draw(args.viewport(), |c, gl| {
-            // Clear the screen.
-            clear(GREEN, gl);
-
-            let transform = c
-                .transform
-                .trans(x, y)
-                .rot_rad(rotation)
-                .trans(-25.0, -25.0);
-
-            // Draw a box rotating around the middle of the screen.
-            rectangle(RED, square, transform, gl);
-        });
-    }
-
-    fn update(&mut self, args: &UpdateArgs) {
-        // Rotate 2 radians per second.
-        self.rotation += 2.0 * args.dt;
+        Ok(MainState { bird })
     }
 }
 
-fn main() {
-    // Change this to OpenGL::V2_1 if not working.
-    let opengl = OpenGL::V3_2;
+struct Bird {
+    x: f32,
+    y: f32,
+    gravity: f32,
+    lift: f32,
+    velocity: f32,
+    circle: graphics::Mesh
+}
 
-    // Create a Glutin window.
-    let mut window: Window = WindowSettings::new("spinning-square", [200, 200])
-        .graphics_api(opengl)
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
-
-    // Create a new game and run it.
-    let mut app = App {
-        gl: GlGraphics::new(opengl),
-        rotation: 0.0,
-    };
-
-    let mut events = Events::new(EventSettings::new());
-    while let Some(e) = events.next(&mut window) {
-        if let Some(args) = e.render_args() {
-            app.render(&args);
-        }
-
-        if let Some(args) = e.update_args() {
-            app.update(&args);
+impl Bird {
+    fn new(circle: graphics::Mesh) -> Bird {
+        Bird {
+            x: 100.0,
+            y: 250.0,
+            gravity: 0.6,
+            lift: -15.0,
+            velocity: 0.0,
+            circle
         }
     }
+
+    fn jump(&mut self) -> () {
+        self.velocity = if self.velocity + self.lift > -5.0 { -5.0 } else { self.velocity + self.lift };
+    }
+
+    fn update(&mut self) -> () {
+        self.y += self.velocity;
+        self.velocity = if self.velocity + self.gravity > 8.0 { 8.0 } else { self.velocity + self.gravity };
+
+
+    }
+}
+
+impl event::EventHandler<ggez::GameError> for MainState {
+    fn key_down_event(
+            &mut self,
+            _ctx: &mut Context,
+            input: ggez::input::keyboard::KeyInput,
+            _repeated: bool,
+        ) -> Result<(), ggez::GameError> {
+            Ok(if input.keycode == Some(KeyCode::Space) {
+                self.bird.jump();
+            })
+    }
+
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        self.bird.update();
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        let mut canvas =
+            graphics::Canvas::from_frame(ctx, graphics::Color::from([0.1, 0.2, 0.3, 1.0]));
+
+        canvas.draw(&self.bird.circle, Vec2::new(self.bird.x, self.bird.y));
+
+        canvas.finish(ctx)?;
+
+        Ok(())
+    }
+}
+
+pub fn main() -> GameResult {
+    let cb = ggez::ContextBuilder::new("super_simple", "ggez");
+    let (mut ctx, event_loop) = cb.build()?;
+    let state = MainState::new(&mut ctx)?;
+    event::run(ctx, event_loop, state)
 }
